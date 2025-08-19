73ab34ee079480db707730d4a63673a7feb04cd8
JoaoV <joaovcarneirog@gmail.com>
2025-08-19 08:48:03 -0300

    next e react redefinidos

--- wbmb/app/page.js
+++ wbmb/app/page.js
@@ -1,10 +1,5 @@
 export default function Home() {
   return (
-    <><html lang="pt-br"></html><div>
-      <h1>
-        <br />
-        teste
-      </h1>
-    </div></>
-  );
+    <div>teste novo</div>
+  )
 }
