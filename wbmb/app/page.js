343101ccad0e99b81520589d8abdc955753317b6
JoaoV <joaovcarneirog@gmail.com>
2025-08-19 08:57:53 -0300

    au

--- wbmb/app/page.js
+++ wbmb/app/page.js
@@ -1,5 +1,6 @@
 export default function Home() {
   return (
-    <div>teste novo</div>
+    <><div>teste novo</div>
+      <img>https://avatars.githubusercontent.com/u/99694870?v=4&size=40</img></>
   )
 }
