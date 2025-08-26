b4c5bcfe6fac5b2bef350f6fff567c6abe5ba627
JoaoV <joaovcarneirog@gmail.com>
2025-08-26 09:53:22 -0300

    mod

--- wbmb/app/page.js
+++ wbmb/app/page.js
@@ -2,10 +2,13 @@ export default function Home() {
   return (
     <>
     <header>
+      <title>Web Mobile</title>
     <h1>teste novo</h1>
       </header>
+      <body>
       <img src='https://avatars.githubusercontent.com/u/99694870?v=4&size=40' 
       width={100} height={100}/>
+      </body>
       </>
   )
 }
