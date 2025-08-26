0666139ded870dc8eb82959abf90293faf7f6042
JoaoV <joaovcarneirog@gmail.com>
2025-08-26 10:40:49 -0300

    teste

--- wbmb/app/page.js
+++ wbmb/app/page.js
@@ -5,8 +5,16 @@ export default function Home() {
       <title>Web Mobile</title>
     <h1>teste novo</h1>
       </header>
+      <a href='https://wbmbprojeto-oc1u.vercel.app/'target='_blank'>
       <img src='https://avatars.githubusercontent.com/u/99694870?v=4&size=40' 
       width={100} height={100}/>
+      </a>
+      <p>
+      <a href='https://github.com/joaovcguerra/wbmbprojeto.git'target='_blank'>Meu Repositório</a>
+      </p>
+      <p>
+        <img src='C:\Users\joaov\OneDrive\Documentos\cadeira de web e mobile\wbmbprojeto\wbmb\public'/>
+      </p>
       </>
   )
 }
